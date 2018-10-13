37cba9d4034e7bf83634895b271549fd61ba254a
haitao5 <haitao5@126.com>
2018-10-13 21:18:17 +0800

    add source code

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 
 ## 说明
-
-
+根据[博客](https://www.jianshu.com/p/c4f35dfe1a1f)的示例，爬取One网页的信息，并将爬取信息保存的Excel中。
+保存信息包含：日期，内容，图片链接
 
 ## 步骤
 
@@ -11,7 +11,9 @@
   1. 在 learn_scrapy 文件夹中执行：*python -m venv py_env*
   2. 运行 *py_env\Scripts\activate*，激活虚拟环境
 
-
+#### 更新源码
+  1. 下载源码并分析
+  2. 
 
 
 
---
+++ scrapy_main.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#import argparse
+# import re
+from multiprocessing import Pool
+import requests
+import bs4
+import time
+import json
+# import io
+
+root_url = 'http://wufazhuce.com'
+
+def get_url(num):
+    # 拼接URL
+    return root_url + '/one/' + str(num)
+
+def get_urls(num):
+    # 使用map(function, iterable, ...) 函数根据提供的函数对指定序列做映射
+    urls = map(get_url, range(100,100+num))
+    return urls
+
+def get_data(url):
+    dataList = {}
+    response = requests.get(url)    # 读取网页
+    if response.status_code != 200:
+        return {'noValue': 'noValue'}
+    #print(response.text)
+    # 解析    
+    soup = bs4.BeautifulSoup(response.text,"html.parser")
+    dataList["index"] = soup.title.string[4:7]
+    for meta in soup.select('meta'):
+        if meta.get('name') == 'description':
+            dataList["content"] = meta.get('content')
+    dataList["imgUrl"] = soup.find_all('img')[1]['src']
+    return dataList
+
+
+if __name__=='__main__':
+    """
+    使用 pool.map(function, iter)将迭代器中的数字作为参数依次传入函数中
+    """
+
+    pool = Pool(4)   # 创建线程池资源
+    dataList = []
+    urls = get_urls(10)  # 映射URL列表
+    
+    # 
+    # 记录运行时间
+    start = time.time()
+    dataList = pool.map(get_data, urls)
+    end = time.time()
+    print ('use: %.2f s' % (end - start))
+
+    # 保存到文件中
+    for i in dataList:
+        print(i)
+    jsonData = json.dumps({'data':dataList})
+    with open('data.json', 'w') as outfile:
+        json.dump(jsonData, outfile)
